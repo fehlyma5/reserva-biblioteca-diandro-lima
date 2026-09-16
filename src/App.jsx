602271c34b5eee324ac14499a42cb3045b447081
fehlyma5 <fehlyma5@gmail.com>
2026-09-16 02:18:41 -0300

    corrigindo rotas, dependencias e importações do react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Route, Routes } from "react-router";
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AcervoPage from "./pages/AcervoPage";
 import NovoLivroPage from "./pages/NovoLivroPage";
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router"; // <-- Importação do roteador
-import './index.css'
-import App from './App.jsx'
-import { BooksProvider } from './context/BooksContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from './App.jsx';
+import { BooksProvider } from './context/BooksContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +12,5 @@ createRoot(document.getElementById('root')).render(
         <App />
       </BooksProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
